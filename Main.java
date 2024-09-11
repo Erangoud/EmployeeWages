890250a341e08684b809cee4d5c93b2f337a495e
Erangoud <egoud83@gmail.com>
2024-09-11 09:56:55 +0530

    added functionalites emp att

---
+++ Main.java
@@ -0,0 +1,17 @@
+package com;
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("===== WELCOME TO EMPLOYEE WAGE COMPUTATION ======");
+
+        int empCheck = (int) Math.floor(Math.random() * 10) % 2;
+        if(empCheck == 0)
+        {
+            System.out.println("Employee is Present");
+        }
+        else
+        {
+            System.out.println("Employee is Absent");
+        }
+
+    }
+}
